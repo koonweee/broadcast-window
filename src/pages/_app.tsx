e75b6724b1421183a0d71a45ea459817116363c4
Jeremy Tan <jeremy.tan@newfront.com>
2025-04-12 19:17:27 -0700

    Title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,15 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
 import Header from "@/components/Header";
+import Head from 'next/head';
 
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <SessionProvider session={session}>
+      <Head>
+        <title>Broadcast Window</title>
+        <meta name="description" content="Stream viewer for broadcast-box" />
+      </Head>
       <div className="bg-gray-800 min-h-screen flex flex-col">
         <Header />
         <main className="flex-1">
